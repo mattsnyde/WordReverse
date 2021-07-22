e2369e3f186ba3a7d8f52d1d272985ffffe8494f
mattsnyde <matthewx.snyder@gmail.com>
2021-07-22 12:40:48 -0700

    Reversed String project done

--- js/site.js
+++ js/site.js
@@ -1,3 +1,28 @@
-function helloWorld(){ //Function is container that holds code that is only ran when the function is called.
-    alert('Hello World')
+//Get string from the page, controller function
+function getString(){
+    document.getElementById('alert').classList.add('invisible') /*Add invisible class if it is not already present on the div*/
+    const userString = document.getElementById('userString').value
+     let reversedString = reverseString(userString)
+     displayString(reversedString)
+ }
+
+//Reverse the string, logic function
+function reverseString(userString){
+    let reversedString = [];
+
+    //Reversing the string using a for loop
+    for(let i = userString.length - 1; i >= 0; i--){ //userString.length-1 is the very last position in a string
+      reversedString += userString[i] //Add each letter from userString to the empty array we declared above.
+    }
+    return reversedString
 }
+
+// //Display reversed string to  user view function
+function displayString(reversedString){
+    //Write message to page and then show the alert box
+    const displayString = document.getElementById('message')
+    displayString.innerHTML = `${reversedString}`
+    document.getElementById('alert').classList.remove('invisible') //Get rid of the invisible class on the alert box so users can see the box now.
+}
+
+ 
